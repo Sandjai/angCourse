c47d95c97d0017cf5858c4a7f4f72074181fa85b
Grigory Semakin <grinsdot1@gmail.com>
2019-11-17 14:54:08 +0300

    start with Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+
+
+
+@NgModule({
+ exports: [
+  MatButtonModule,
+  MatToolbarModule
+ ]
+  
+ 
+ 
+})
+export class SharedModule { }
